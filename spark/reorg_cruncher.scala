cb703d212ade2cd98c56af5e6dc9037049f76ef0
实验台边吃早饭 <kzmain@users.noreply.github.com>
2020-09-21 06:31:10 +0800

    Update reorg_cruncher.scala

--- spark/reorg_cruncher.scala
+++ spark/reorg_cruncher.scala
@@ -6,6 +6,7 @@ def reorg(datadir :String)
                        load(datadir + "/person.*csv.*")
   val knows    = spark.read.format("csv").option("header", "true").option("delimiter", "|").option("inferschema", "true").
                        load(datadir + "/knows.*csv.*")
+  val loc_df = person.select("personId", "locatedIn").cache()
   var nknows = knows
 .join(loc_df.withColumnRenamed("locatedIn", "ploc"),    "personId")
 .join(loc_df.withColumnRenamed("locatedIn", "floc")
@@ -32,7 +33,7 @@ var nperson = person_list
     .drop("locatedIn")
     .withColumnRenamed("bday", "birthday")
 
-nperson.write.format("parquet").mode("overwrite").save("person.parquet")
+nperson.write.format("parquet").mode("overwrite").save(datadir + "/person.parquet")
 
 
 val interest  = spark.read.format("csv")
@@ -43,7 +44,7 @@ val interest  = spark.read.format("csv")
 
 var ninterest = interest.join(person_list, "personId")
 
-ninterest.write.format("parquet").mode("overwrite").save("interest.parquet")
+ninterest.write.format("parquet").mode("overwrite").save(datadir + "/interest.parquet")
 
   val t1 = System.nanoTime()
   println("reorg time: " + (t1 - t0)/1000000 + "ms")
@@ -53,9 +54,9 @@ def cruncher(datadir :String, a1 :Int, a2 :Int, a3 :Int, a4 :Int, lo :Int, hi :I
 {
    val t0 = System.nanoTime()
 
-  val interest = spark.read.format("parquet").load("interest.parquet").cache()
-val person   = spark.read.format("parquet").load("person.parquet").cache()
-val knows    = spark.read.format("parquet").load("knows.parquet").cache()
+  val interest = spark.read.format("parquet").load(datadir + "/interest.parquet").cache()
+val person   = spark.read.format("parquet").load(datadir + "/person.parquet").cache()
+val knows    = spark.read.format("parquet").load(datadir + "/knows.parquet").cache()
 
   // Filter Person between birthdays
 val target = person.filter($"birthday" >= lo && $"birthday" <= hi).drop("birthday")
