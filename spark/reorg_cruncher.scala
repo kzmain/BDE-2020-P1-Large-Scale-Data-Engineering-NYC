849c2b3c44ed8db15256b4919ebdb208abbb1353
实验台边吃早饭 <main@kai.sh>
2020-09-22 03:10:02 +0800

    Update reorg_cruncher.scala

--- spark/reorg_cruncher.scala
+++ spark/reorg_cruncher.scala
@@ -1,7 +1,5 @@
 import org.apache.spark.sql.types.ByteType
 import org.apache.spark.sql.types.ShortType
-import scala.collection.mutable.ListBuffer
-
 def reorg(datadir :String) 
 {
   val t0 = System.nanoTime()
@@ -14,7 +12,6 @@ def reorg(datadir :String)
                        .drop("creationDate")
                        .drop("locationIP")
                        .drop("browserUsed")
-                       .withColumn("month", (month($"birthday")).cast(ByteType))
                        .withColumn("bday", (month($"birthday")*100 + dayofmonth($"birthday")).cast(ShortType))
                        .drop("birthday")
                        .cache()
@@ -46,21 +43,16 @@ def reorg(datadir :String)
     person_list.cache()
 
     //Remove none-useful person
-        println("REORG: REMOVE NONE_USEFULE PERSON")
-    val np = person.join(person_list, "personId").drop("locatedIn").cache()
-
-    var lm = 1;
-    var hm = 12;
-
-  for( lm <- 1 to hm){
-         np.filter($"month" === lm).drop("month").write.format("parquet").mode("overwrite").save(datadir + "/kmonth=" + lm)
-  }
+    println("REORG: REMOVE NONE_USEFULE PERSON")
+    person.join(person_list, "personId").drop("locatedIn").write.format("parquet").mode("overwrite").save(datadir + "/person_kk.parquet")
     
     val interest = spark.read.format("csv").option("header", "true").option("delimiter", "|").option("inferschema", "true").
                        load(datadir + "/interest.*csv.*").cache()
     //Remove none-useful interests
     println("REORG: REMOVE NONE_USEFULE INTEREST")                   
-    interest.join(person_list, "personId").write.format("parquet").mode("overwrite").save(datadir + "/interest_kk.parquet")
+    interest.join(person_list, "personId")
+    .groupBy("interest").agg(collect_list("personId").as("personId"))
+    .write.format("parquet").mode("overwrite").save(datadir + "/interest_kk.parquet")
 
   val t1 = System.nanoTime()
   println("reorg time: " + (t1 - t0)/1000000 + "ms")
@@ -69,19 +61,9 @@ def reorg(datadir :String)
 def cruncher(datadir :String, a1 :Int, a2 :Int, a3 :Int, a4 :Int, lo :Int, hi :Int) :org.apache.spark.sql.DataFrame =
 {
   val t0 = System.nanoTime()
-
-  var lm = lo / 100;
-  var hm = hi / 100;
-
-  var name = ListBuffer[String]()
-
-  for( lm <- 1 to hm){
-         name  += datadir + "/kmonth=" + lm
-  }
-
     
   val person   = spark.read.format("parquet").option("header", "true").option("delimiter", "|").option("inferschema", "true").
-                   load(name: _*).cache()  
+                   load(datadir + "/person_kk.parquet").cache()
 
   val interest = spark.read.format("parquet").option("header", "true").option("delimiter", "|").option("inferschema", "true").
                    load(datadir + "/interest_kk.parquet").cache()
@@ -89,8 +71,9 @@ def cruncher(datadir :String, a1 :Int, a2 :Int, a3 :Int, a4 :Int, lo :Int, hi :I
   val knows    = spark.read.format("parquet").option("header", "true").option("delimiter", "|").option("inferschema", "true").
                        load(datadir + "/knows_kk.parquet").cache()
   
-  val focus    = interest.filter($"interest" isin (a1, a2, a3, a4)).
-                          withColumn("nofan", $"interest".notEqual(a1))
+  val focus    = interest.filter($"interest" isin (a1, a2, a3, a4))
+                          .withColumn("personId", explode($"personId"))
+                          .withColumn("nofan", $"interest".notEqual(a1))
                           .groupBy("personId")
                           .agg(count("personId") as "score", min("nofan") as "nofan")
 
